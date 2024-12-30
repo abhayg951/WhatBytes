8572be30b0880abe393ebc7c126a7cb8b42ee01a
Abhay Gupta <insuabhay951@gmail.com>
2024-12-30 22:21:06 +0530

    bug fix

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("forgot-password/", auth_views.PasswordResetView.as_view(template_name="forget_password.html"), name="password_reset"),
     path("password-reset-done/", auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name="password_reset_done"),
     path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name="password_reset_confirm"),
-    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name="password_reset_complete"),
+    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name="password_reset_complete"),
     
     path("password-change/", auth_views.PasswordChangeView.as_view(template_name="password_change.html"), name="password_change"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
